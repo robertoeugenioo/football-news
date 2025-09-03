0dfeb75a9723c329420cbd1a56c93e417b6c3976
robertoeugenioo <robertotoes12345@gmail.com>
2025-09-03 15:08:46 +0700

    fix requirement.txt dan flag DEBUG

--- football_news/settings.py
+++ football_news/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure--4^g!_%^%bum$7g*etwn-2=@*$qh4do)dj-1f%@m!h#olrtqs0
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","roberto-eugenio-footballnews.pbp.cs.ui.ac.id"]
 
